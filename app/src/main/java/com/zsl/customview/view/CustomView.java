254c1238bf040016fc2822d70c3c5b1fc49111a1
zhengsonglan <1300326388@qq.com>
2015-06-17 16:58:13 +0800

    画了一个圆环

--- app/src/main/java/com/zsl/customview/view/CustomView.java
+++ app/src/main/java/com/zsl/customview/view/CustomView.java
@@ -1,6 +1,10 @@
 package com.zsl.customview.view;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
 import android.view.View;
 
 /**
@@ -8,8 +12,41 @@ import android.view.View;
  * 自定义View
  */
 public class CustomView extends View {
+	Paint paint;
 
 	public CustomView(Context context) {
 		super(context);
+	}
+
+	public CustomView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		initPaint();
+	}
+
+	//初始化画笔
+	private void initPaint() {
+		paint=new Paint();
+		//设置抗锯齿
+		paint.setAntiAlias(true);
+		//设置颜色
+		paint.setColor(Color.BLUE);
+		/**
+		 * 设置画笔样式
+		 * 1.Paint.Style.FILL  填充
+		 * 2.Paint.Style.FILL_AND_STROKE 填充并描边
+		 * 3.Paint.Style.STROKE 描边
+		 */
+		paint.setStyle(Paint.Style.STROKE);
+		//设置描边的粗细，当值为0时，也有一个像素
+		paint.setStrokeWidth(10);
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+		//绘制圆环
+		canvas.drawCircle(300,300,200,paint);
+
+
 	}
 }
