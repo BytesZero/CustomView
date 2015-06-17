deeaf4cb0fbbf101995f9a66b0303aba789a7998
zhengsonglan <1300326388@qq.com>
2015-06-17 16:34:05 +0800

    initView

---
+++ app/src/main/java/com/zsl/customview/MainActivity.java
@@ -0,0 +1,33 @@
+package com.zsl.customview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+import com.zsl.customview.view.CustomView;
+
+public class MainActivity extends AppCompatActivity {
+
+	LinearLayout ll_showview;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		initView();
+
+
+	}
+
+	private void initView() {
+		ll_showview= (LinearLayout) findViewById(R.id.main_ll_showview);
+
+		ll_showview.addView(new CustomView(this));
+
+	}
+
+
+}
---
+++ app/src/main/java/com/zsl/customview/view/CustomView.java
@@ -0,0 +1,15 @@
+package com.zsl.customview.view;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by zsl on 15/6/17.
+ * 自定义View
+ */
+public class CustomView extends View {
+
+	public CustomView(Context context) {
+		super(context);
+	}
+}
